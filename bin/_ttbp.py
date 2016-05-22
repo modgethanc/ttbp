1827d8467f4b9e5f3aeb72dc5454379151a62051
endorphant <endorphant@tilde.town>
2016-05-21 22:18:25 -0400

    big documentation pass

--- bin/_ttbp.py
+++ bin/_ttbp.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python
 
+'''
+ttbp: tilde town blogging platform
+(also known as the feels engine)
+a console-based blogging program developed for tilde.town
+copyright (c) 2016 ~endorphant (endorphant@tilde.town)
+
+_ttbp.py:
+the beta version of the main console interface
+
+GNU GPL BOILERPLATE:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+the complete codebase is available at:
+https://github.com/modgethanc/ttbp
+'''
+
 import os
 import random
 import tempfile
